94ea610d1df10fa6ef1e31f3f7c1bd230a3a5e31
shivam-singh-code <shivamsingh97166@gmail.com>
2024-06-18 22:02:56 +0530

    video user model added

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -6,13 +6,13 @@ const asyncHandler = (requestHandler) => {
 
 export { asyncHandler };
 
-// const asyncHandler = (fn = async (req, res, next) => {
-//   try {
-//     await fn(req, res, next);
-//   } catch (error) {
-//     res.status(error.code || 500).json({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
